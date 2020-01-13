9d35b9ee44f71917a5903d9bb52a69a2738ecc7f
Haig Armen <haig@liftstudios.ca>
2020-01-13 14:00:47 -0800

    updates to exercises 1-6

--- 3/sketch.js
+++ 3/sketch.js
@@ -1,6 +1,6 @@
 /** Name: Haig Armen
 Course: INTD319
-Assignment: P5.js Basics
+Assignment: Lines from Center
 Blog: https://courses.haigarmen.com/intd319-2020
 Description:
 **/
@@ -13,7 +13,27 @@ function setup() {
     background(0); // this makes the background black
 }
 function draw() {
+    // random shade of gray for line colour
     stroke(random(255));
+
+    // A.
     // start lines in the middle of the screen (width/2, height/2)
+    // first we'll try it without variables
     line(width/2, height/2, random(width), random(height));
+
+    // B.
+    // if we want to remember the random numbers used to draw the line
+    // we'll need to create some variables
+    // let lineX = random(width);
+    // let lineY = random(height);
+    // line(width/2, height/2, lineX, lineY);
+
+    // C.
+    // now that we've saved the random numbers as variables we can recall them
+    // to draw other shapes too
+    // noFill();
+    // ellipseMode(CENTER);
+    // ellipse(lineX, lineY, 10,10);
+
+
 }
--- 4/sketch.js
+++ 4/sketch.js
@@ -16,12 +16,23 @@ function draw() {
     noStroke();
     fill(0);
     // vertical bars
-    rect(40,0,10, height*.66);
+    rect(40,0,10, height-300);
+    rect(300,0,10, height);
+    rect(400,0,10, height-300);
+    rect(600,0,10, height);
+    rect(630,0,10, height);
+    rect(670,0,10, height);
+    rect(770,0,10, height);
+    rect(870,0,10, height);
+
+    // vertical bars
+    rect(0,100,width, 10);
 
     // colour blocks
     fill(255,0,0);
     rect(0,0,40, 200);
-    // rect(,);
+    fill(0,0,255);
+    rect(300,0,100, 100);
     // rect(,);
 
 
