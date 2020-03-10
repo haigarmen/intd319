3961410548fa27d69fd744c157d3cc035277dc89
Haig Armen <haig@liftstudios.ca>
2020-03-10 13:35:56 -0700

    updated example 11

--- 11/sketch.js
+++ 11/sketch.js
@@ -7,15 +7,14 @@ Description:
 
 function setup() {
     canvas = createCanvas(windowWidth, windowHeight);
-    strokeWeight(8);
 }
 
 function draw() {
     background(204);
-    textSize(220);
+    textSize(20);
     // tracking mouse coordinates
-    text("X: " + mouseX, 50, height / 2);
-    text("Y: " + mouseY, 50, height / 2 + 210);
+    // text("X: " + mouseX, 50, height / 2);
+    // text("Y: " + mouseY, 50, height / 2 + 210);
 
     // draw with mouse coordinates
     // ellipse(mouseX, mouseY, 10, 10);
@@ -28,15 +27,33 @@ function draw() {
 
 
     // simple buttons
-
-    // noStroke();
-    // if (mouseX < width/2) {
-    //     rect(0, 0, width/2, height); // Left
+    drawButton("hello",10,10);
+    drawButton("goodbye",200,10);
+    drawButton("maoz",300,10);
+    drawButton("karsen",400,10);
+    noStroke();
+    // fill(255,0,0);
+    // if (mouseX < width/2 && mouseY < height/2) {
+    //     rect(0, 0, width/2, height/2); // top Left
+    // } else if (mouseX > width/2 && mouseY < height/2) {
+    //     rect(width/2, 0, width/2, height/2); // top Right
+    // } else if (mouseX < width/2 && mouseY > height/2) {
+    //     rect(0, height/2, width/2, height/2); // bottom left
     // } else {
-    //     rect(width/2, 0, width/2, height); // Right
+    //     rect(width/2, height/2, width/2, height/2); // bottom left
     // }
 }
 
-function drawButton() {
-
+function drawButton(label,x,y) {
+    fill(255);
+    rect(x,y,100,100);
+    fill(0);
+    textAlign(CENTER);
+    text(label, x+100/2, y+100/2);
+    if (mouseX > x && mouseX < x+100 & mouseY >y && mouseY <y+100) {
+        fill(255,0,0);
+        rect(x,y,100,100);
+        fill(255);
+        text(label, x+100/2, y+100/2);
+    }
 }
